dc642a63a6fb6721f49ea2e16b56562089308a12
Mads Finnerup <Finnerup1@gmail.com>
2018-10-04 10:11:47 +0200

    Initial Commit

---
+++ TrackTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackTrack
+//
+//  Created by Mads Finnerup on 04/10/2018.
+//  Copyright © 2018 Mads Finnerup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
