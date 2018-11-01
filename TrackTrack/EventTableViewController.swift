915223ca37e55d4b0968914ddf8f360c31e418b0
Mads Finnerup <Finnerup1@gmail.com>
2018-11-01 10:47:52 +0100

    Add navigation bar

--- TrackTrack/EventTableViewController.swift
+++ TrackTrack/EventTableViewController.swift
@@ -11,21 +11,55 @@ import UIKit
 
 struct cellData {
     let cell : Int!
-    let text : String!
-    let image : UIImage!
+    let eventName : String!
+    let sportName : String!
+    let date : String!
+    let races : String!
+    let competitors : String!
+    
+    let flagImage : UIImage!
+    let sportImage : UIImage!
+    let dateImage : UIImage!
+    let raceImage : UIImage!
+    let compotitorsImage : UIImage!
 }
 
 class EventTableViewController: UITableViewController {
-
+    var arrayOfCellData = [cellData]()
+    
+    override func viewDidLoad() {
+    
+        arrayOfCellData = [cellData(cell: 1, eventName: "EuroSail 2018", sportName: "Sailing", date: "20-27.2018", races: "48 Races", competitors: "12 Competitors", flagImage: #imageLiteral(resourceName: "Eurosail.png"), sportImage: #imageLiteral(resourceName: "Eurosail.png"), dateImage: #imageLiteral(resourceName: "Eurosail.png"), raceImage: #imageLiteral(resourceName: "Eurosail.png"), compotitorsImage: #imageLiteral(resourceName: "Eurosail.png"))]
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+   return arrayOfCellData.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if arrayOfCellData[indexPath.row].cell == 1{
+            let cell = Bundle.main.loadNibNamed("EventTableViewCell", owner: self, options: nil)?.first as! EventTableViewCell
+            cell.flagImage.image = arrayOfCellData[indexPath.row].flagImage
+            cell.sportLabel.text = arrayOfCellData[indexPath.row].sportName
+            cell.nameOfEvent.text = arrayOfCellData[indexPath.row].eventName
+            cell.dateLabe.text = arrayOfCellData[indexPath.row].date
+            cell.competitorsLabel.text = arrayOfCellData[indexPath.row].competitors
+            cell.raceLabel.text = arrayOfCellData[indexPath.row].races
+            return cell
+            
+        }
+        else {
+            let cell = Bundle.main.loadNibNamed("EventTableViewCell", owner: self, options: nil)?.first as! EventTableViewCell
+            cell.flagImage.image = arrayOfCellData[indexPath.row].flagImage
+            cell.sportLabel.text = arrayOfCellData[indexPath.row].sportName
+            cell.nameOfEvent.text = arrayOfCellData[indexPath.row].eventName
+            cell.dateLabe.text = arrayOfCellData[indexPath.row].date
+            cell.competitorsLabel.text = arrayOfCellData[indexPath.row].competitors
+            cell.raceLabel.text = arrayOfCellData[indexPath.row].races
+            return cell
+        }
     }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        <#code#>
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
     }
     
     
