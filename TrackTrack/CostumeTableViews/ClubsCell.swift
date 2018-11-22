f37bb3f9f507ab56e28aadea505086aa6f394bd9
Iman Chelhi <iman_1997@live.dk>
2018-11-22 13:36:08 +0100

    Clubs should work

---
+++ TrackTrack/CostumeTableViews/ClubsCell.swift
@@ -0,0 +1,17 @@
+//
+//  ClubsCell.swift
+//  TracTrac
+//
+//  Created by Iman Chelhi on 22/11/2018.
+//  Copyright © 2018 Mads Finnerup. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ClubsCell: UITableViewCell {
+
+    @IBOutlet var logoImage: UIImageView!
+    
+    @IBOutlet var logoImage2: UIImageView!
+}
